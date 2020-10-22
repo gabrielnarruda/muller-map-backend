68e332992cc972d75f1bbcc67a35a4150ac16480
Gabriel Arruda <gabrielaugusto.arruda@gmail.com>
2020-10-21 22:20:13 -0300

    ajust variaveis de ambiente e remoção de lixo

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -2,6 +2,7 @@
 
 import connexion
 
+from swagger_server.configuration.environment_variables import HOST, PORT
 from swagger_server.configuration.exceptions import generic_render, Unauthorized
 # from swagger_server.configuration.logger import create_logger
 from swagger_server.configuration.context_handler import before_request
@@ -15,8 +16,8 @@ def main():
     app = connexion.FlaskApp(__name__, specification_dir='./swagger/')
     app.add_api('swagger.yaml')
     app.add_error_handler(Unauthorized, generic_render)
-    #app.app.before_request(before_request)
-    app.run(port=8080)
+    # app.app.before_request(before_request)
+    app.run(host=HOST, port=PORT)
 
 
 if __name__ == '__main__':
--- swagger_server/models/produtos_model.py
+++
@@ -1,7 +0,0 @@
-import peewee
-from swagger_server.models.base_model import BaseModel
-
-
-class tb_produto(BaseModel):
-    id = peewee.IntegerField()
-    json_data = peewee.CharField()
