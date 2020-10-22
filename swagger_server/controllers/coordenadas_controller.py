9bcb861ba06af7563b6b4ad65761ec5fa66cb443
Gabriel Arruda <gabrielaugusto.arruda@gmail.com>
2020-10-21 22:08:19 -0300

    adicionado GET coordenadas

--- swagger_server/controllers/coordenadas_controller.py
+++ swagger_server/controllers/coordenadas_controller.py
@@ -1,7 +1,13 @@
+from swagger_server.views.coordenadas_view import insert_nova_coordenada, listar_todas_coordenadas
 
 
-def post_cria_coordenada(**kwargs):
-    return kwargs
+def post_cria_coordenada(coordenada):
+    id_coordenada = insert_nova_coordenada(coordenada)
+    if type(id_coordenada) == int:
+        coordenada['id'] = id_coordenada
+        return coordenada
+    else:
+        return (f"Error>>> {id_coordenada}")
 
 def get_lista_coordenadas():
-    return "ok"
+    return listar_todas_coordenadas()
--- swagger_server/models/coordenadas_model.py
+++ swagger_server/models/coordenadas_model.py
@@ -7,8 +7,8 @@ from swagger_server.models.usuarios_model import tb_usuario
 class tb_coordenadas(BaseModel):
     id = peewee.IntegerField()
     nm_local = peewee.TextField()
-    lat = peewee.DecimalField()
-    lon = peewee.DecimalField()
-    indice_radiacao = peewee.DecimalField()
-    id_usuario = peewee.ForeignKeyField(tb_usuario, 'id')
+    lat = peewee.FloatField()
+    lon = peewee.FloatField()
+    indice_radiacao = peewee.FloatField()
+    id_usuario = peewee.ForeignKeyField(tb_usuario, column_name='id_usuario')
     created_at = peewee.DateField()
