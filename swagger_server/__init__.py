e7e01dacb01cf035fcec177ebfeeb4761ef00efc
Gabriel Arruda <gabrielaugusto.arruda@gmail.com>
2020-10-21 22:26:41 -0300

    trash delete

--- swagger_server/__init__.py
+++ swagger_server/__init__.py
@@ -1,10 +1,2 @@
-from swagger_server.configuration.data_mock.data_mock_runner import create_table_mock, insert_table_data_mock, \
-    delete_table_mock
-
-
-
-delete_table_mock()
-create_table_mock()
-insert_table_data_mock()
 
 
